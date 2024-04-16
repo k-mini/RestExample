a8fc3f78e25d6a1b1f739878fa8ffb1e216fdd40
Kwang min <rhkdals195@naver.com>
2024-04-16 22:13:05 +0900

    rest config 수정

--- src/main/java/com/example/rest/RestConfig.java
+++ src/main/java/com/example/rest/RestConfig.java
@@ -9,15 +9,10 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -29,27 +24,33 @@ public class RestConfig {
         RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
 
         RestTemplate restTemplate = restTemplateBuilder
-                .setReadTimeout(Duration.ofMillis(3000)) // 읽기 시간
+//                .setReadTimeout(Duration.ofMillis(3000)) // 읽기 시간
                 .setConnectTimeout(Duration.ofMillis(3000)) // 연결 시간
-//                .requestFactory(() -> {
-//
-//                    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-//                    factory.setConnectTimeout(5000);
-//                    factory.setConnectTimeout(3000);
-//                    HttpClient httpClient = HttpClientBuilder.create()
-//                            .setConnectionManager(createHttpClientConnectionManager())
-//                            .build();
-//                    factory.setHttpClient(httpClient);
-//                    BufferingClientHttpRequestFactory buffedFactory
-//                            = new BufferingClientHttpRequestFactory(factory);
-//                    return factory;
-//                })
-                .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+                .requestFactory(() -> {
+                    // 3.0 이상 requestFactory 사용할거면 위에 setReadTimeOut 주석처리
+                    // setReadTimeOut 해당 속성은 삭제됨
+                    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+                    factory.setConnectionRequestTimeout(5000);
+                    factory.setConnectTimeout(3000);
+
+                    factory.setHttpClient(createHttpClient());
+
+                    BufferingClientHttpRequestFactory buffedFactory
+                            = new BufferingClientHttpRequestFactory(factory);
+                    return factory;
+                })
+//                .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
                 .build();
 
         restTemplate.getInterceptors().add(new RestTemplateLoggingRequestInterceptor());
-
         return restTemplate;
+    }
+
+    private HttpClient createHttpClient() {
+        return org.apache.hc.client5.http.impl.classic
+                .HttpClientBuilder.create()
+                .setConnectionManager(createHttpClientConnectionManager())
+                .build();
     }
 
     private HttpClientConnectionManager createHttpClientConnectionManager() {
